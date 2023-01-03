e5ff382fc40d9fa9f6aa4f56947960242e71afac
chaimHL <1302906470@qq.com>
2023-01-03 19:47:21 +0800

    首页顶部tab

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:lbluebook_logistics/navigator/lb_navigator.dart';
+import 'package:lbluebook_logistics/page/home_tab_page.dart';
+import 'package:underline_indicator/underline_indicator.dart';
 
 import '../model/video_model.dart';
+import '../util/color.dart';
+import '../util/font.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,11 +15,14 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage>
-    with AutomaticKeepAliveClientMixin {
+    with AutomaticKeepAliveClientMixin, TickerProviderStateMixin {
   var listener;
+  late TabController _controller;
+  var tabs = ['推荐', '热门', '追播', '影视', '搞笑', '日常'];
   @override
   void initState() {
     super.initState();
+    _controller = TabController(length: tabs.length, vsync: this);
     LbNavigator.getInstance().addListener(listener = (current, pre) {
       print('首页监听:current:${current.page}');
       print('首页监听:pre:${pre.page}');
@@ -36,16 +43,19 @@ class _HomePageState extends State<HomePage>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
       body: Column(
         children: [
-          const Text('首页'),
-          MaterialButton(
-            onPressed: () {
-              LbNavigator.getInstance().onJumpTo(RouteStatus.detail,
-                  args: {'videoMo': VideoModel(100)});
-            },
-            child: const Text('详情'),
+          Container(
+            color: Colors.white,
+            padding: const EdgeInsets.only(top: 30),
+            child: _tabBar(),
+          ),
+          Flexible(
+            child: TabBarView(
+                controller: _controller,
+                children: tabs.map((tab) {
+                  return HomeTabPage(name: tab);
+                }).toList()),
           )
         ],
       ),
@@ -54,4 +64,26 @@ class _HomePageState extends State<HomePage>
 
   @override
   bool get wantKeepAlive => true;
+
+  _tabBar() {
+    return TabBar(
+        controller: _controller,
+        isScrollable: true,
+        labelColor: Colors.black,
+        indicator: const UnderlineIndicator(
+            strokeCap: StrokeCap.round,
+            borderSide: BorderSide(color: ColorPrimary, width: 3),
+            insets: EdgeInsets.only(left: 16, right: 16)),
+        tabs: tabs.map<Tab>((tab) {
+          return Tab(
+            child: Padding(
+              padding: const EdgeInsets.only(left: 4, right: 4),
+              child: Text(
+                tab,
+                style: const TextStyle(fontSize: FontSizeDefalut),
+              ),
+            ),
+          );
+        }).toList());
+  }
 }
